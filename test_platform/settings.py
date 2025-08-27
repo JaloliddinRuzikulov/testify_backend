4c52e31de789c10b54fc764dfd75ab7a21d546b1
JaloliddinRuzikulov <ruzikulov@tuit.uz>
2025-08-27 15:59:18 +0500

    add allow host

--- test_platform/settings.py
+++ test_platform/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["r2.uzbmb.uz"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -149,6 +149,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "r1.uzbmb.uz",
     "http://127.0.0.1:3030",
     "http://localhost:3030",
     "http://127.0.0.1:3033",
